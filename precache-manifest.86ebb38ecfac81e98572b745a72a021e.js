4b3bf714f67408a7a837a71b3618d8528d380071
dev <dev@mail.com>
2020-05-09 18:02:20 +0900

    Updates

---
+++ precache-manifest.86ebb38ecfac81e98572b745a72a021e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f89ab2e5ad538e3746eb162a860d03",
+    "url": "/Drevo_Platonov/index.html"
+  },
+  {
+    "revision": "91a508a4e1a022cd72cc",
+    "url": "/Drevo_Platonov/static/css/main.5eed1b2c.chunk.css"
+  },
+  {
+    "revision": "e2111e9430962cbd70c4",
+    "url": "/Drevo_Platonov/static/js/2.2765c11a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Drevo_Platonov/static/js/2.2765c11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a508a4e1a022cd72cc",
+    "url": "/Drevo_Platonov/static/js/main.18628f91.chunk.js"
+  },
+  {
+    "revision": "3c953633234ce3366143",
+    "url": "/Drevo_Platonov/static/js/runtime-main.8f41805b.js"
+  },
+  {
+    "revision": "248dde74ce1af3476d8e145268e05073",
+    "url": "/Drevo_Platonov/static/media/Alekcandra_F.248dde74.png"
+  },
+  {
+    "revision": "f6449a2a7a52d1a4b292260152ea6b8a",
+    "url": "/Drevo_Platonov/static/media/Cutar.f6449a2a.png"
+  },
+  {
+    "revision": "6ee9de3542ecd216ce029c0dee44ee22",
+    "url": "/Drevo_Platonov/static/media/E_A_Markov.6ee9de35.png"
+  },
+  {
+    "revision": "dd8058689529033751f0e534e0006629",
+    "url": "/Drevo_Platonov/static/media/Fedor_Ivanovich_Platonov.dd805868.png"
+  },
+  {
+    "revision": "60e68221c41ea46dfa23d751c61cab0c",
+    "url": "/Drevo_Platonov/static/media/Gavril_Dmitri_Pavlov.60e68221.png"
+  },
+  {
+    "revision": "ec01330795703dce02b4e5a75b954557",
+    "url": "/Drevo_Platonov/static/media/Innokentiy.ec013307.png"
+  },
+  {
+    "revision": "6b35bfee3f72538542e6414d36ae8e35",
+    "url": "/Drevo_Platonov/static/media/Ivan_F_Platonov.6b35bfee.png"
+  },
+  {
+    "revision": "df98fd2c9a460770c08babf00f7075b6",
+    "url": "/Drevo_Platonov/static/media/N_MarkovI.df98fd2c.png"
+  },
+  {
+    "revision": "e0e6c4f6acd374db00396367d8a2b93b",
+    "url": "/Drevo_Platonov/static/media/Nikolay_D_Pavlov.e0e6c4f6.png"
+  },
+  {
+    "revision": "6811d848300908b48865b847a846aff3",
+    "url": "/Drevo_Platonov/static/media/Nikolay_F_Platonov.6811d848.png"
+  },
+  {
+    "revision": "95497b428a8ca368c8d83ee34a00b3a7",
+    "url": "/Drevo_Platonov/static/media/Platonovu.95497b42.png"
+  }
+]);
